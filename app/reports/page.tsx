192b0b2a1c595320c4e624eaca7d52618027d068
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-06 08:33:37 +0200

    Set up basic pages

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,22 +1,22 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Reports() {
   return (
     <>
       <header className="flex justify-between items-center mb-4">
-        <h1 className="text-2xl">Home</h1>
+        <h1 className="text-2xl">Reports</h1>
         <div>
+          <Link
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none mx-2"
+            href="/"
+          >
+            home
+          </Link>
           <Link
             className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none mx-2"
             href="/users"
           >
             users
-          </Link>
-          <Link
-            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none mx-2"
-            href="/reports"
-          >
-            reports
           </Link>
         </div>
       </header>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Users() {
   return (
     <>
       <header className="flex justify-between items-center mb-4">
-        <h1 className="text-2xl">Home</h1>
+        <h1 className="text-2xl">Users</h1>
         <div>
           <Link
             className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none mx-2"
             href="/users"
           >
-            users
+            home
           </Link>
           <Link
             className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none mx-2"
