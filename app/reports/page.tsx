0d372e957a7c7cff805630f2594609df54d97a8e
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-06 09:33:31 +0200

    Add table for reports

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
-async function getReports() {
-  const response = await fetch("http://localhost:4000/reports/all");
+type Report = {
+  id: number;
+  make: string;
+  model: string;
+  price: number;
+  year: number;
+  lng: number;
+  lat: number;
+  mileage: number;
+  approved: boolean;
+};
 
-  const reports = await response.json();
+async function getReports() {
+  const response = await fetch("http://localhost:4000/reports/all", {
+    next: { revalidate: 1000 },
+  });
+
+  const reports: Promise<Report[]> = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
   return reports;
 }
@@ -32,9 +50,61 @@ export default async function Reports() {
         </div>
       </header>
       <ul className="pl-4">
-        {reports.map((report: any) => (
-          <li key={report.id}>{report.make}</li>
-        ))}
+        <div className="overflow-hidden">
+          <table className="min-w-full text-left text-sm font-light">
+            <thead className="border-b font-medium dark:border-neutral-500">
+              <tr>
+                <th scope="col" className="px-6 py-4">
+                  #
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Make
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Model
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Year
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Mileage
+                </th>
+                <th scope="col" className="px-6 py-4">
+                  Approved
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {reports.map((report) => (
+                <tr
+                  key={report.id}
+                  className="border-b dark:border-neutral-500"
+                >
+                  <td className="whitespace-nowrap px-6 py-4 font-medium">
+                    {report.id}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">{report.make}</td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {report.model}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {report.price}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">{report.year}</td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {report.mileage}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {report.approved ? "✅" : "❌"}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </ul>
     </>
   );
