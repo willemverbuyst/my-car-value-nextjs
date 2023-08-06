8cc5c1870f11fd662d4de1dc1da624309349528c
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-06 13:35:37 +0200

    Create form for sing in

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,31 @@
+"use client";
 import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const validationSchemaSignIn = z.object({
+  email: z.string().email().min(1, { message: "Email is required" }),
+  password: z.string().min(1, { message: "Password is required" }),
+});
+
+export type FormFieldsSignIn = z.infer<typeof validationSchemaSignIn>;
+
+const defaultValuesSignIn: FormFieldsSignIn = {
+  email: "",
+  password: "",
+};
 
 export default function Home() {
+  const { handleSubmit, register, reset } = useForm<FormFieldsSignIn>({
+    defaultValues: defaultValuesSignIn,
+    mode: "onChange",
+  });
+
+  const onSubmit: SubmitHandler<FormFieldsSignIn> = async (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <>
       <header className="flex justify-between items-center mb-4">
@@ -20,6 +45,31 @@ export default function Home() {
           </Link>
         </div>
       </header>
+      <div className="flex justify-center">
+        <div className="flex flex-col w-60">
+          <h2 className="text-1xl my-2">Sign in</h2>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex gap-2 flex-col"
+          >
+            <input
+              {...register("email")}
+              type="email"
+              placeholder="email"
+              className="bg-slate-100 rounded px-2 py-1 outline-none text-slate-800"
+            />
+            <input
+              {...register("password")}
+              type="password"
+              placeholder="password"
+              className="bg-slate-100 rounded px-2 py-1 outline-none text-slate-800"
+            />
+            <button className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
