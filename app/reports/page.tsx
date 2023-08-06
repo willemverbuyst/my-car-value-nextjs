f491f9c545653ed2584db269725b3e6f436ccd37
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-06 09:01:21 +0200

    Get reports

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
 
-export default function Reports() {
+async function getReports() {
+  const response = await fetch("http://localhost:4000/reports/all");
+
+  const reports = await response.json();
+
+  return reports;
+}
+
+export default async function Reports() {
+  const reports = await getReports();
+  console.log("reports :>> ", reports);
+
   return (
     <>
       <header className="flex justify-between items-center mb-4">
@@ -20,6 +31,11 @@ export default function Reports() {
           </Link>
         </div>
       </header>
+      <ul className="pl-4">
+        {reports.map((report: any) => (
+          <li key={report.id}>{report.make}</li>
+        ))}
+      </ul>
     </>
   );
 }
